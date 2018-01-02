b921fb39d422dc5be9b65bc3c2a9033c405bd232
guapusing <rendisukirman.rs@gmail.com>
2018-01-02 22:55:38 +0700

    Spenner ke bawah

---
+++ app/src/main/java/com/mobiledev/skydev/widgetspiner/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mobiledev.skydev.widgetspiner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinner1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner1.getSelectedItem()))) {
+                // ToDo when first item is selected
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "Kamu memilih : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
+
+    }
+}
